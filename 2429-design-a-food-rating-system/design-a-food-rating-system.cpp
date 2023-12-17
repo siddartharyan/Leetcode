18393c488e37d4a83af199a8c73d2699b3fa5485
V Sai Siddartha <68399822+siddartharyan@users.noreply.github.com>
2023-12-17 11:44:47 +0530

    Time: 409 ms (69.86%) | Memory: 177.8 MB (19.14%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -19,11 +19,13 @@ class FoodRatings {
 public:
     unordered_map<string,string>foodMap;
     unordered_map<string,int>cuisineMap;
+    unordered_map<string,pair<string,int>>mp1;
     unordered_map<string,priority_queue<Food>>cuisinesToFoodMap;  
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         for(int i=0;i<foods.size();++i){
             foodMap[foods[i]]=cuisines[i];
             cuisineMap[foods[i]]=ratings[i];
+            mp1[foods[i]]=make_pair(cuisines[i],ratings[i]);
             cuisinesToFoodMap[cuisines[i]].push(Food(ratings[i],foods[i]));
         }
     }
@@ -31,12 +33,15 @@ public:
     void changeRating(string food, int newRating) {
         auto cuisine = foodMap[food];
         cuisineMap[food]=newRating;
+        auto x = mp1[food];
+        x.second = newRating;
+        mp1[food]=x;
         cuisinesToFoodMap[cuisine].push(Food(newRating,food));
     }
     
     string highestRated(string cuisine) {
         auto highestRated = cuisinesToFoodMap[cuisine].top();
-        while(cuisineMap[highestRated.food] != cuisinesToFoodMap[cuisine].top().rating){
+        while(mp1[highestRated.food].second != cuisinesToFoodMap[cuisine].top().rating){
             cuisinesToFoodMap[cuisine].pop();
             highestRated = cuisinesToFoodMap[cuisine].top();
         }
