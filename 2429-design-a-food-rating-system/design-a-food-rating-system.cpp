6e2e956c2891285340dc3ff8a6108d1a2f964e7c
V Sai Siddartha <68399822+siddartharyan@users.noreply.github.com>
2023-12-17 11:45:39 +0530

    Time: 338 ms (98.56%) | Memory: 152 MB (99.52%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -17,26 +17,20 @@ class Food{
 
 class FoodRatings {
 public:
-    unordered_map<string,string>foodMap;
-    unordered_map<string,int>cuisineMap;
     unordered_map<string,pair<string,int>>mp1;
     unordered_map<string,priority_queue<Food>>cuisinesToFoodMap;  
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         for(int i=0;i<foods.size();++i){
-            foodMap[foods[i]]=cuisines[i];
-            cuisineMap[foods[i]]=ratings[i];
             mp1[foods[i]]=make_pair(cuisines[i],ratings[i]);
             cuisinesToFoodMap[cuisines[i]].push(Food(ratings[i],foods[i]));
         }
     }
     
     void changeRating(string food, int newRating) {
-        auto cuisine = foodMap[food];
-        cuisineMap[food]=newRating;
         auto x = mp1[food];
         x.second = newRating;
         mp1[food]=x;
-        cuisinesToFoodMap[cuisine].push(Food(newRating,food));
+        cuisinesToFoodMap[x.first].push(Food(newRating,food));
     }
     
     string highestRated(string cuisine) {
