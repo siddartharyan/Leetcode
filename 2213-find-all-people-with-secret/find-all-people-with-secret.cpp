6e5dab5f46b9dddaf44b80a4de3ee134db7d8c12
V Sai Siddartha <68399822+siddartharyan@users.noreply.github.com>
2024-02-24 11:48:50 +0530

    Time: 1055 ms (19.68%) | Memory: 200.4 MB (33.20%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -1,71 +1,27 @@
-class UnionFind{
-    private:
-        vector<int>p;
-        vector<int>r;
-    public:
-        UnionFind(int n){
-            for(int i=0;i<n;++i) p.push_back(i);
-            r.resize(n,0);
-        }
-
-        int findPar(int x){
-            if(p[x] == x) return x;
-            return p[x]=findPar(p[x]);
-        }
-
-        void merge(int x,int y){
-            int px = findPar(x);
-            int py = findPar(y);
-            if(px != py){
-                if(r[px] > r[py]){
-                    p[py]=px;
-                }else if(r[px] < r[py]){
-                    p[px]=py;
-                }else{
-                    p[py]=px;
-                    ++r[px];
-                }
-            }
-        }
-        
-        bool isConnected(int x,int y){
-            return findPar(x) == findPar(y);
-        }
-
-        void reset(int x){
-            p[x]=x;
-            r[x]=0;
-        }
-};
-
 class Solution {
 public:
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int f) {
-        auto cmp = [](vector<int>&a,vector<int>&b){
-            return a[2]<b[2];
-        };
-        sort(meetings.begin(),meetings.end(),cmp);
-        map<int,vector<pair<int,int>>>mp;
-        for(auto x:meetings){
-            mp[x[2]].emplace_back(x[0],x[1]);
-        }
-        UnionFind uf(n);
-        uf.merge(f,0);
+    vector<int> findAllPeople(int n, vector<vector<int>>& m, int f) {
         vector<int>ans;
-        for(auto x:mp){
-            for(auto y:x.second){
-                uf.merge(y.first,y.second);
-            }
-            for(auto y:x.second){
-                if(!uf.isConnected(y.first,0)){
-                    uf.reset(y.first);
-                    uf.reset(y.second);
-                }
+        unordered_map<int,vector<pair<int,int>>>mp;
+        for(auto x:m){
+            mp[x[0]].push_back({x[1],x[2]});
+            mp[x[1]].push_back({x[0],x[2]});
+        }
+        vector<int>vis(n,false);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
+        p.push({0,0});
+        p.push({0,f});
+        while(!p.empty()){
+            auto [time,per] = p.top();
+            p.pop();
+            vis[per]=true;
+            for(auto [p1,t1]:mp[per]){
+                if(!vis[p1] && t1>=time) p.push({t1,p1});
             }
         }
 
         for(int i=0;i<n;++i){
-            if(uf.isConnected(i,0)) ans.push_back(i);
+            if(vis[i]) ans.push_back(i);
         }
         return ans;
     }
