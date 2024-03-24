201ab21408ac50b14aaaf21454ad75cf2478b1ca
V Sai Siddartha <68399822+siddartharyan@users.noreply.github.com>
2024-03-24 12:02:53 +0530

    Time: 13 ms (35.95%) | Memory: 42 MB (74.47%) - LeetSync

---
+++ 1203-print-in-order/print-in-order.java
@@ -0,0 +1,27 @@
+class Foo {
+    Semaphore s1,s2;
+    public Foo() {
+        s1 = new Semaphore(0);
+        s2 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        s1.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        s1.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        s2.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        s2.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
