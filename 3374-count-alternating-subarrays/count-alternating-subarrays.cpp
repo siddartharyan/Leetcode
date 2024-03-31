45352cb01f6836cee71f91448516582627936e19
V Sai Siddartha <68399822+siddartharyan@users.noreply.github.com>
2024-03-31 10:39:37 +0530

    Time: 85 ms (85.71%) | Memory: 116.2 MB (14.29%) - LeetSync

---
+++ 3374-count-alternating-subarrays/count-alternating-subarrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countAlternatingSubarrays(vector<int>& nums) {
+        long long ans = 1;
+        long s = 1;
+        for(int i=1;i<nums.size();++i){
+            s = (nums[i] == nums[i-1])?1:s+1;
+            ans += s;
+        }
+        return ans;
+    }
+};
